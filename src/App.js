3018367179e65303f6e685186278e00c84615a1d
Guillaume Clabaut <123895070+GClabaut@users.noreply.github.com>
2024-07-19 13:57:08 +0200

    Made basic css loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import normalize from './styles/normalize.css';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Loader from '../components/Loader'
 
 const Home = () => {
 
+  const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState([true]);
   useEffect(() => {
-    fetch('http://localhost:8000/api/tasks')
-    .then((res) => res.json())
-    .then((data) => {return data['hydra:member']})
-  }, [tasks])
+    if (isLoading) {
+      fetch('http://localhost:8000/api/tasks')
+      .then((res) => res.json())
+      .then((data) => {
+        setTasks(data['hydra:member']);
+        setIsLoading(!isLoading);
+      });
+    }
+  }, [isLoading]);
 
-  return (
-    <div>
-      <span>
+  console.log(tasks)
 
-      </span>
-    </div>
-  )
-}
+  return !isLoading ? (
+    tasks?.map((task) => {
+      return (
+      <div key={task?.id}>
+        <h2>{task?.titre}</h2>
+      </div>
+      );
+    })
+    ) : <div><Loader /></div>;
+  }
 
 export default Home;
